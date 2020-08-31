fbcd9e32bdf2e412eb732e4454e74f1961fbf917
Yirzaraji <not.iremy@gmail.com>
2020-08-31 14:14:36 +0200

    test

--- jsonexo/index.php
+++ jsonexo/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
     <!-- Optional theme -->
 
     <title>Ajax Jquery & fetch</title>
@@ -18,6 +19,10 @@
     </div>
     <div class="row">
         <div id="jsonLoader2" class="col-12 text-center"> </div>   
+    </div>
+
+    <div class="row">
+        <div id="test3" class="col-12 text-center">e</div> 
     </div>
 </div>
 
--- jsonexo/js/script.js
+++ jsonexo/js/script.js
@@ -2,12 +2,38 @@ fetch('joueurs.json')
     .then(response => response.json())
 
 .then(data => {
-    console.log(data)
+    //console.log(data)
     const jsonResponse2 = document.getElementById('jsonLoader2');
     const test2 = document.getElementById('test2');
 
-    jsonResponse2.innerHTML = '<br>' + data.nom + '</br>' + data.pseudo + '<br>' + data.xp;
-    test2.innerHTML = '<img src="' + data.urlavatar + '">';
+    /* jsonResponse2.innerHTML = '<br>' + data.nom + '</br>' + data.pseudo + '<br>' + data.xp;
+    test2.innerHTML = '<img src="' + data.urlavatar + '">'; */
+
+    for (let i = 0; i < data.length; i++){
+
+        console.log(data[i])
+        console.log(data[i].nom)
+
+        //marche pas correctement
+        test2.innerHTML = data[i].nom
+
+        let newDiv = document.createElement("div");
+        newDiv.id = "div" + i;
+        newDiv.className = "card";
+        //console.log(newDiv.id)
+
+        newDiv.innerHTML = data[i].nom + '<br>' + data[i].pseudo + '<br>' + data[i].xp;
+        // et lui donne un peu de contenu
+        var newContent = document.createTextNode('');
+        // ajoute le nœud texte au nouveau div créé
+        newDiv.appendChild(newContent);
+        
+        // ajoute le nouvel élément créé et son contenu dans le DOM
+        var currentDiv = document.getElementById('div1');
+        document.body.insertBefore(newDiv, currentDiv);
+        
+    }
+
 })
 
 .catch(error => console.error(error));
